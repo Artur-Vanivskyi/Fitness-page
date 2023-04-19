1cda8602fe5f8e09db76dbbbf35750269a4f32ab
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-04-18 22:59:52 -0400

    added contaioners from mui material

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { Box, Stack, Button, TextFiled, Typography } from "@mui/material";
 
 function SearchExercises() {
   return (
