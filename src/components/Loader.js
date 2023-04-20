cbbf9ac903e0eab8638f04f5d4eb0913ddeace37
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-04-20 13:00:43 -0400

    cleaned loader and navbar

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,18 +1,11 @@
-import React from "react";
-import { Stack } from "@mui/material";
-import { InfinitySpin } from "react-loader-spinner";
+import React from 'react';
+import { Stack } from '@mui/material';
+import { InfinitySpin } from 'react-loader-spinner';
 
-function Loader() {
-  return (
-    <Stack
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      width="100%"
-    >
-      <InfinitySpin color="gray" />
-    </Stack>
-  );
-}
+const Loader = () => (
+  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+    <InfinitySpin color="grey" />
+  </Stack>
+);
 
 export default Loader;
